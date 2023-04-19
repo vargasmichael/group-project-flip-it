4ce2dd70ea9c0a63b1450ef858bbb4626a5e4881
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-04-19 16:15:18 -0600

    all the changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,31 @@
+
+import Head from 'next/head'
+import Image from 'next/image'
 import { Inter } from 'next/font/google'
+// import styles from '@/styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+import Tiles_on_Board from "./Tiles_on_Board";
+import Each_tile from "./Each_tile";
+// import HOF from './HOF';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+function Home() {
+//  const [gameTiles, setgameTiles] = useState([]); //part 1 late - create const [] to hold the tiles, import useState, replace consolelog in fetch with setgameTiles (will be an empty array)
+
+//   useEffect(() => {
+//     fetch("http://localhost:8000/tiles") //part 1 early -main db; plural variable
+//       .then((res) => res.json())
+//       .then((gameTiles) => setgameTiles(gameTiles)); // conlog - full array of obj was present set implies it will watch tiles
+//   }, []);
+
   return (
     <div>
-      <h1>Hello, World!</h1>
+      <Each_tile />
+      <Tiles_on_Board />
     </div>
-  )
+  );
 }
-  
+
+export default Home;
